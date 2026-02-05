cee2c508af5a370306e55f622063dd95b27f2652
joaocosta2395 <joao.costa2395@gmail.com>
2026-02-05 11:06:29 -0300

    main.py

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ app = FastAPI()
 def health():
     return {"ok": True}
 
-@app.get("/status-rodada")
-def status_rodada():
+@app.get("/rodada")
+def rodada():
     return {
         "status_rodada": "mercado_aberto",
         "rodada": 1
     }
 
-@app.get("/jogos")
-def jogos():
+@app.get("/dados")
+def dados():
     return {
         "jogos": [
             {
@@ -23,12 +23,7 @@ def jogos():
                 "favoravel_ataque": True,
                 "favoravel_defesa": False
             }
-        ]
-    }
-
-@app.get("/jogadores")
-def jogadores():
-    return {
+        ],
         "jogadores": [
             {
                 "nome": "Pedro",
@@ -36,10 +31,8 @@ def jogadores():
                 "posicao": "ATAQUE",
                 "casa_fora": "Casa",
                 "preco": 23.5,
-                "status": "provavel",
-                "motivo_simples": "Finaliza muito e joga em casa",
-                "capitao_viavel": True,
-                "plano_b": ["Gabigol"]
+                "motivo": "Finaliza muito e joga em casa",
+                "capitao": True
             }
         ]
     }
